0ffcb84f7e79d1996281e2d90faf7cca8909926a
sangwoo Park <sw0082@naver.com>
2020-10-30 17:28:22 +0900

    Input data

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+if __name__ == "__main__":
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    Regexp = '(a+b)*'
+    # prestate, symbol, changestate
+    delta = [[0, 'a', 1],
+             [2, 'b', 3],
+             [4, 'eps', 0],
+             [4, 'eps', 2],
+             [1, 'eps', 5],
+             [3, 'eps', 5],
+             [6, 'eps', 4],
+             [6, 'eps', 7],
+             [5, 'eps', 4],
+             [5, 'eps', 7]]
+    startstate = 0
+    finalstate = {7}
 
+    Q = list(range(0, len(delta)))
+    sigmalist = set([i[1] for i in delta])
+    sigmalist.discard('eps')
+    changestatelist = set([i[2] for i in delta])
+    prestatelist = set([i[0] for i in delta])
+    statelist = changestatelist | prestatelist
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("e-NFA to DFA")
+    print("Regexp: " + Regexp)
+    print("Q:", statelist)
+    print("Sigma:", sigmalist)
+    print("q0:", startstate)
+    print("F:", finalstate)
+    for x, y, z in delta:
+        print('Delta(' + str(x) + ', ' + y + ') = ' + str(z))
